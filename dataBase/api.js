0992cd4f9da2263a9fb07f38bef972d9403a6cfa
longer567 <1652423708@qq.com>
2019-12-19 23:41:24 +0800

    feat: dataBase finish

---
+++ dataBase/api.js
@@ -0,0 +1,66 @@
+
+/**
+ * 
+ * @param {*} collection 
+ * @param {Array} objectArray An array constituted of multiple objects
+ * @param {(res) => {}} callback Do something by res
+ */
+const insertManyDocuments = (collection, objectArray, callback) => {
+    collection
+      .insertMany(objectArray, (err, result) => callback(result));
+}
+
+/**
+ * 
+ * @param {*} collection 
+ * @param {Object} objectOne An object
+ * @param {(res) => {}} callback Do something by res
+ */
+const insertOneDocument = (collection, objectOne, callback) => {
+    collection
+      .insertOne(objectOne, (err, result) => callback(result));
+}
+
+const findAllDocuments = (collection, callback) => {
+    collection.find({})
+      .toArray((err, docs) => callback(docs));
+}
+
+/**
+ * 
+ * @param {*} collection 
+ * @param {Object} object Finding a line by an object
+ * @param {(res) => {}} callback Do something by res
+ */
+const findLineDocument = (collection, object, callback) => {
+    collection
+      .find(object)
+        .toArray((err, docs) => callback(docs))
+}
+
+/**
+ * 
+ * @param {*} collection 
+ * @param {object} aim Want to update aim object
+ * @param {object} to Provided replacement
+ * @param {(res) => {}} callback Do something by res
+ */
+const updateOneDocument = (collection, aim, to, callback) => {
+    collection
+        .updateOne(aim, {
+            $set: to
+        }, (err, result) => callback(result))
+}
+
+/**
+ * 
+ * @param {*} collection 
+ * @param {object} object Want to delete object
+ * @param {(res) => {}} callback Do something by res
+ */
+const removeOneDocument = (collection, object, callback) => {
+    collection
+      .deleteOne(object, (err, res) => callback(res))
+}
+
+module.exports = { insertManyDocuments, findAllDocuments, findLineDocument, updateOneDocument, removeOneDocument, insertOneDocument }
--- dataBase/index.js
+++ dataBase/index.js
@@ -2,12 +2,40 @@ const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
 const config = require('config')
 
-const { url, dbName } = config.get("dataBase")
+const API = require('./api')
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-    assert.equal(null, err)
-    console.log("Serve connect success!")
+const {
+    url,
+    dbName
+} = config.get("dataBase")
 
-    const db = client.db(dbName)
-    client.close()
-})
+const db = (documents) => {
+    MongoClient.connect(url, {
+        useUnifiedTopology: true
+    }, (err, client) => {
+        assert.equal(null, err)
+        console.log("Serve connect success!")
+    
+        const db = client.db(dbName)
+        const collection = db.collection(documents);
+    
+        const obj = {
+            'title': 'activity1',
+            'data': new Date(),
+            'content': {
+                a: 111,
+                b: 222
+            }
+        }
+        
+        API.insertOneDocument(collection, obj, (res) => {
+            console.log(res)
+        })
+
+        client.close()
+    })
+}
+
+db('documents')
+
+
