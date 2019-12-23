185e56764397652ee15867d60793a65e7a89b5e0
longer567 <1652423708@qq.com>
2019-12-23 16:15:25 +0800

    feat: finish editer json and inputTree convert

--- public/test.js
+++ public/test.js
@@ -8,23 +8,36 @@ var dataJson = {
 }
 var inputData = {
     "a": {
-        title: "活动标题1",
-        value: "111"
+        "title": "活动标题1",
+        "value": "111"
     },
     "b": {
-        title: "活动标题2",
-        value: "222"
+        "title": "活动标题2",
+        "value": "222"
     },
     "c": {
-        title: "活动标题4与5的对象",
-        value: {
+        "title": "活动标题4与5的对象",
+        "value": {
             "d": {
-                title: "活动标题4",
-                value: "333"
+                "title": "活动标题4",
+                "value": "333"
             },
             "f": {
-                title: "活动标题5",
-                value: "444"
+                "title": "活动标题5",
+                "value": "444"
+            },
+            "g": {
+                "title": "活动标题6与7的对象",
+                "value": {
+                    "k": {
+                        "title": "活动标题6",
+                        "value": "666"
+                    },
+                    "l": {
+                        "title": "活动标题7",
+                        "value": "777"
+                    }
+                }
             }
         }
     }
--- views/editer/editer.js
+++ views/editer/editer.js
@@ -1,7 +1,5 @@
 $(document).on('click', '.json-before', function (e) {
-    var nowJsonDom = $($(e.target).parents()[0]);
-    console.log(nowJsonDom.attr('index'))
-    console.log(nowJsonDom.attr('cj'))
+    let nowJsonDom = $($(e.target).parents()[0]);
     if (nowJsonDom.attr('show') === 'true') {
         nowJsonDom.children('.json-content').css('height', '0')
         nowJsonDom.children('.json-before').css('transform', 'rotate(0)')
@@ -14,13 +12,69 @@ $(document).on('click', '.json-before', function (e) {
 })
 
 // 创建点击
-$(document).on('click', '.addItem', function (e) {
-    console.log($('.editer-data').val())
+$(document).on('click', '.addItem-input', function (e) {
+    try {
+        const val = JSON.parse($('.editer-data').val())
+        const inputTreeCreated = valTreeCreate(val)
+
+        $('.editer-input').append(`<form class='formInput'>${inputTreeCreated}</form>`)
+    } catch {
+        console.log('请填写正确的json格式')
+    }
+
+})
+// 生产json点击
+$(document).on('click', '.addItem-json', function (e) {
+    try {
+        // const val = $('.formInput').serializeArray()
+        const childArr = $('.formInput').children()
+        const inputCreateJson = treeJsonCreate(childArr)
+
+        $('.editer-result').text(JSON.stringify(inputCreateJson, null, 4))
+
+    } catch (err) {
+        console.log(err)
+    }
+
 })
 
-// request(api.addItem, {
-//     itemName: 'test1',
-//     itemContent: {
+function valTreeCreate(val) {
+    let domTemp = ''
+    for (let i in val) {
+        if (['string', 'number'].includes(val[i].value) || ['string', 'number'].includes(typeof val[i].value)) {
+            domTemp += `<div class='item'>
+                    <div class='input-title'>
+                        ${val[i].title} (${i})
+                    </div>
+                    <input class='input-content'
+                        type='text'
+                        value='${val[i].value}'
+                        name='${i}'>
+                    </input>
+                </div>`
+        } else if (['object'].includes(val[i].value) || ['object'].includes(typeof val[i].value)) {
+            domTemp += `<div class='json' show='false'>${val[i].title} (${i})
+                    <div class = 'json-before'></div>
+                    <div class = 'json-content' name='${i}'>
+                        ${valTreeCreate(val[i].value)}
+                    </div>
+                </div>`
+        }
+    }
+    return domTemp
+}
 
-//     }
-// }).then(res=> console.log(res))
+function treeJsonCreate(childArr) {
+    let jsonTemp = {}
+    for (let i in childArr) {
+        const className = childArr[i].className
+        if (className === 'item') {
+            const itemChildren = $($(childArr[i]).children('.input-content')[0])
+            jsonTemp[itemChildren.attr('name')] = itemChildren.val()
+        } else if (className === 'json') {
+            const itemChildren = $($(childArr[i]).children('.json-content')[0])
+            jsonTemp[itemChildren.attr('name')] = treeJsonCreate(itemChildren.children())
+        }
+    }
+    return jsonTemp
+}
